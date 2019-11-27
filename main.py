MISC - fixed asy-sonata underflow

main.py
@@ -7,9 +7,9 @@ import numpy as np
 
 from arguments import get_args
 
-from maopy.push_sum_gossip_diging import PushDIGing
-from maopy.push_sum_gossip_gradient_descent import PushSumSubgradientDescent
-from maopy.push_sum_gossip_extra import ExtraPush
+from maopy.push_diging import PushDIGing
+from maopy.gradient_push import GradientPush
+from maopy.extra_push import ExtraPush
 from maopy.asy_sonata import AsySONATA
 from utils.distributed import Printer
 from utils.distributed import load_peers
@@ -45,41 +45,39 @@ def main(args):
             args.data_file_name, args.rank, args.size, printer=printer)
 
     # Initialize multi-agent optimizer
-    if args.alg == 'asy-sonata':
-        optimizer = AsySONATA(objective=objective,
-                              sub_gradient=gradient,
-                              arg_start=arg_start,
-                              peers=peers,
-                              step_size=args.lr,
-                              termination_condition=args.num_steps,
-                              in_degree=2,
-                              log=True)
-        loggers = optimizer.minimize()
-        l_argmin_est = loggers['argmin_est']
-        np.savez_compressed(args.fpath,
-                            argmin_est=l_argmin_est)
-    else:
+    if args.alg != 'asy-sonata':
         if args.alg == 'gp':
-            OptimizerClass = PushSumSubgradientDescent
+            optimizer = GradientPush(objective=objective,
+                                     sub_gradient=gradient,
+                                     arg_start=arg_start,
+                                     asynch=args.asynch,
+                                     peers=peers,
+                                     step_size=args.lr,
+                                     max_itr=args.max_itr,
+                                     max_time_sec=args.max_time_sec,
+                                     in_degree=in_degree,
+                                     tau_proc=args.tau,
+                                     log=True)
         elif args.alg == 'pd':
-            OptimizerClass = PushDIGing
+            optimizer = PushDIGing(objective=objective,
+                                   sub_gradient=gradient,
+                                   arg_start=arg_start,
+                                   peers=peers,
+                                   step_size=args.lr,
+                                   max_itr=args.max_itr,
+                                   in_degree=in_degree,
+                                   log=True)
         elif args.alg == 'ep':
-            OptimizerClass = ExtraPush
-        optimizer = OptimizerClass(
-            objective=objective,
-            sub_gradient=gradient,
-            arg_start=arg_start,
-            synch=(not args.asynch),
-            peers=peers,
-            step_size=args.lr,
-            terminate_by_time=args.asynch,
-            termination_condition=args.num_steps,
-            log=True,
-            out_degree=out_degree,
-            in_degree=in_degree,
-            all_reduce=False)
+            optimizer = ExtraPush(objective=objective,
+                                  sub_gradient=gradient,
+                                  arg_start=arg_start,
+                                  peers=peers,
+                                  step_size=args.lr,
+                                  max_itr=args.max_itr,
+                                  in_degree=in_degree,
+                                  log=True)
 
-        # Log and save results
+        # -- log and save results
         loggers = optimizer.minimize()
         l_argmin_est = loggers['argmin_est'].history
         l_ps_w = loggers['ps_w'].history
@@ -87,16 +85,19 @@ def main(args):
         np.savez_compressed(args.fpath,
                             argmin_est=l_argmin_est,
                             ps_w=l_ps_w)
-
-    # # Load global objective
-    # objective, _, _, arg_min = load_least_squares(args.data_file_name, 0, 1)
-    # true_obj = objective(arg_min)
-    # start_obj = objective(arg_start)
-    # final_obj = objective(loggers['argmin_est'].gossip_value)
-    # printer.stdout(
-    #     '(truth: %.4E)(final: %.4E)(start: %.4E)' % (
-    #         true_obj, final_obj, start_obj)
-    # )
+    else:
+        optimizer = AsySONATA(objective=objective,
+                              sub_gradient=gradient,
+                              arg_start=arg_start,
+                              peers=peers,
+                              step_size=args.lr,
+                              max_time_sec=args.max_time_sec,
+                              in_degree=in_degree,
+                              log=True)
+        loggers = optimizer.minimize()
+        l_argmin_est = loggers['argmin_est'].history
+        np.savez_compressed(args.fpath,
+                            argmin_est=l_argmin_est)
 
     printer.stdout('fin.')
 

maopy/gossip/pull_gossip.py
@@ -32,8 +32,8 @@ class PullGossipAverager(object):
     def __init__(self, peers=[(UID+1) % SIZE]):
         """ Initialize the distributed averaging settings """
 
-        # Break on all numpy warnings
-        np.seterr(all='raise')
+        # # Break on all numpy warnings
+        # np.seterr(all='raise')
 
         self.peers = peers
 

maopy/gossip/push_sum_gossip.py
@@ -33,8 +33,8 @@ class PushSumAverager(object):
     def __init__(self, peers=[(UID+1) % SIZE], in_degree=SIZE):
         """ Initialize the distributed averaging settings """
 
-        # Break on all numpy warnings
-        np.seterr(all='raise')
+        # # Break on all numpy warnings
+        # np.seterr(all='raise')
 
         self.peers = peers
         self.out_degree = len(self.peers)
@@ -54,7 +54,7 @@ class PushSumAverager(object):
         column['out_p'] = out_p
         return column
 
-    def push_messages_to_peers(self, peers, ps_w, ps_n):
+    def push_messages_to_peers(self, peers, consensus_column, ps_w, ps_n):
         """
         Send scaled push sum numerator and push sum weights to peers.
 
@@ -64,7 +64,7 @@ class PushSumAverager(object):
         :type ps_n: float
         :rtype: void
         """
-        for i, peer_uid in enumerate(peers):
+        for cc_w, peer_uid in zip(consensus_column, peers):
 
             # -- check if last message to peer was sent
             done_indices = []
@@ -76,8 +76,7 @@ class PushSumAverager(object):
                     del self.out_reqs[peer_uid][index]
 
             # -- send message to peer
-            push_message = np.append(ps_n / (self.out_degree + 1.),
-                                     ps_w / (self.out_degree + 1.))
+            push_message = np.append(ps_n * cc_w, ps_w * cc_w)
             req = COMM.Ibsend(push_message, dest=peer_uid, tag=1352)
             self.out_reqs[peer_uid].append(req)
 
@@ -114,8 +113,8 @@ class PushSumAverager(object):
                      'ps_n': np.array[float] or float)
         """
 
+        ps_n = np.zeros(gossip_value.size, dtype=np.float64)
         ps_w = 0
-        ps_n = 0
         num_rcvd = 0
         for _ in range(self.in_degree):
 
@@ -124,7 +123,8 @@ class PushSumAverager(object):
             # -- timeout to avoid deadlocks
             start_time = time.time()
             break_flag = False
-            while not COMM.Iprobe(source=MPI.ANY_SOURCE, status=info, tag=1352):
+            while not COMM.Iprobe(source=MPI.ANY_SOURCE, status=info,
+                                  tag=1352):
                 if time.time() > start_time + 10:
                     break_flag = True
                     break
@@ -162,9 +162,12 @@ class PushSumAverager(object):
 
         # -- push-messages to peers
         if not just_probe:
-            self.push_messages_to_peers(self.peers, ps_w, ps_n)
-            ps_n *= 1. / (self.out_degree + 1)
-            ps_w *= 1. / (self.out_degree + 1)
+            column = self.make_stochastic_weight_column()
+            out_p = column['out_p']  # outgoing mixing weights (vector)
+            lo_p = column['lo_p']  # loop-back mixing weight (scalar)
+            self.push_messages_to_peers(self.peers, out_p, ps_w, ps_n)
+            ps_n *= lo_p
+            ps_w *= lo_p
 
         if asynch:
             rcvd = self.recieve_asynchronously(ps_n)

maopy/gossip/robust_push_gossip.py
@@ -1,5 +1,6 @@
 """
-Robust Push Gossip Averaging class for parallel averaging using column stochastic mixing
+Robust Push Gossip Averaging class for parallel averaging using column
+stochastic mixing
 
 :author: Mido Assran
 :description: Distributed push-based gossip using column stochastic mixing.
@@ -30,8 +31,8 @@ class RobustPushAverager(object):
     def __init__(self, peers=None):
         """ Initialize the distributed averaging settings """
 
-        # Break on all numpy warnings
-        np.seterr(all='raise')
+        # # Break on all numpy warnings
+        # np.seterr(all='raise')
 
         # Set peers to all if not told who peers are
         if not peers:
@@ -62,7 +63,7 @@ class RobustPushAverager(object):
         :type ps_n: float
         :rtype: void
         """
-        for i, peer_uid in enumerate(peers):
+        for cc_w, peer_uid in zip(consensus_column, peers):
 
             # -- check if last message to peer was sent
             done_indices = []
@@ -74,7 +75,7 @@ class RobustPushAverager(object):
                     del self.out_reqs[peer_uid][index]
 
             # -- send message to peer
-            push_message = consensus_column[i]*ps_n
+            push_message = cc_w * ps_n
             if peer_uid not in self.out_buffer:
                 self.out_buffer[peer_uid] = push_message
             else:
@@ -125,7 +126,6 @@ class RobustPushAverager(object):
 
         gossip_value = np.array(gossip_value, dtype=np.float64)
 
-        # Initialize push gossip
         column = self.make_stochastic_weight_column()
         out_p = column['out_p']  # vector
         lo_p = column['lo_p']  # scalar